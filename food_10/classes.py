a6ed160fb9ee6379d390a7211aa15e3f40f49fd7
Jonathan Barrios <hello@jonathanbarrios.com>
2024-06-03 15:38:57 -0500

    add food_10 dataset

---
+++ food_10/classes.py
@@ -0,0 +1,12 @@
+selected_classes = [
+    'sushi', 
+    'strawberry_shortcake', 
+    'pancakes', 
+    'guacamole', 
+    'beef_tartare', 
+    'french_onion_soup', 
+    'churros', 
+    'greek_salad', 
+    'pad_thai', 
+    'pho'
+]
